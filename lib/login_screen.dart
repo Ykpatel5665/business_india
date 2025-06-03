564815345be94d21009dfcd5f84b2928dc663d3c
Yash Patel <86345422+Ykpatel5665@users.noreply.github.com>
2025-06-03 19:07:47 +0530

    added extra avatars

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -67,6 +67,12 @@ class _LoginScreenState extends State<LoginScreen> {
     'assets/avatar2.png',
     'assets/avatar3.png',
     'assets/avatar4.png',
+    'assets/avatar5.png',
+    'assets/avatar6.png',
+    'assets/avatar7.png',
+    'assets/avatar8.png',
+    'assets/avatar9.png',
+    'assets/avatar10.png',
   ];
 
   @override
